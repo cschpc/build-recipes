bae7d6fa32281a45764f6eb2d79f2b2759e62709
Martti Louhivuori <martti.louhivuori@csc.fi>
2016-09-28 13:57:15 +0300

    v1.1.0 on Sisu

---
+++ gpaw/examples/sisu-1.1.0/customize-gnu.py
@@ -0,0 +1,27 @@
+#User provided customizations for the gpaw setup
+import os
+
+# compiler
+compiler = './gcc.py'
+mpicompiler = './gcc.py'
+mpilinker = 'cc'
+extra_compile_args = ['-std=c99']
+
+# libz
+libraries = ['z']
+
+# libxc
+library_dirs += [os.environ['LIBXCDIR'] + '/lib']
+include_dirs += [os.environ['LIBXCDIR'] + '/include']
+libraries += ['xc']
+
+# use ScaLAPACK and HDF5
+scalapack = True
+hdf5 = True
+
+# GPAW defines
+define_macros += [('GPAW_NO_UNDERSCORE_CBLACS', '1')]
+define_macros += [('GPAW_NO_UNDERSCORE_CSCALAPACK', '1')]
+define_macros += [("GPAW_ASYNC",1)]
+define_macros += [("GPAW_MPI2",1)]
+
