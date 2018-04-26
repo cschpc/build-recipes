7fd17491acc517353debdaf25122442b7370ce5d
Martti Louhivuori <martti.louhivuori@csc.fi>
2018-04-26 17:28:53 +0300

    use newer SSL library to fix pip on Sisu

--- scalable-python/load.sh
+++ scalable-python/load.sh
@@ -4,6 +4,7 @@ export PYTHONPATH=$PYTHONHOME/lib
 export PATH=$PYTHONHOME/bin:$PATH
 export MANPATH=$PYTHONHOME/share/man:$MANPATH
 export LD_LIBRARY_PATH=$PYTHONHOME/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/appl/opt/libressl/2.6.4/lib:$LD_LIBRARY_PATH
 
 if [[ $# -gt 0 ]]
 then
