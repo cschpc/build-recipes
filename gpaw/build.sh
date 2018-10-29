655c6468dd4d5338cb9a4f5f21a6520d597dcf32
Martti Louhivuori <martti.louhivuori@csc.fi>
2018-10-29 17:17:17 +0200

    reorganise flow to be similar to gpaw-bundle

--- gpaw/build.sh
+++ gpaw/build.sh
@@ -1,29 +1,25 @@
-# volatile version number
+### GPAW installation script for Sisu (/Taito)
+###   uses --prefix to set a custom installation directory
+
+# version numbers (modify if needed)
 gpaw_version=1.3.0
 
-# get source code
-git clone https://gitlab.com/gpaw/gpaw.git gpaw-$gpaw_version
-cd gpaw-$gpaw_version
-git checkout $gpaw_version
+# installation directory (modify!)
+tgt=/appl/nano/gpaw/$gpaw_version
 
-# load build environment
-module swap PrgEnv-cray PrgEnv-gnu
-module load cray-hdf5-parallel
-module load gpaw-setups/0.9.11271
-module load libxc/3.0.0
+# setup build environment
 module load scalable-python/1.2
-
+module load cray-hdf5-parallel
+module load libxc/3.0.0
 export CRAYPE_LINK_TYPE=dynamic
 export CRAY_ADD_RPATH=yes
 
-# set target directory
-tgt=/appl/nano/gpaw/$gpaw_version
-
-# patch the test suite
+# gpaw
+git clone https://gitlab.com/gpaw/gpaw.git gpaw-$gpaw_version
+cd gpaw-$gpaw_version
+git checkout $gpaw_version
 patch gpaw/test/test.py ../setup/patch-test.diff
 patch gpaw/test/xc/xc.py ../setup/patch-xc.diff
-
-# install and fix dirs
 ln -s ../setup/gcc.py
 python setup.py install --customize=../setup/customize-sisu.py --prefix=$tgt 2>&1 | tee loki-inst
 cd ..
