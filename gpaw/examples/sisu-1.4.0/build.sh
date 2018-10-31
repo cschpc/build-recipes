9cb024dc78e9ecec2418681d1ea473cdabac3e30
Martti Louhivuori <martti.louhivuori@csc.fi>
2018-10-31 17:46:15 +0200

    GPAW 1.4.0 on Sisu

---
+++ gpaw/examples/sisu-1.4.0/build.sh
@@ -0,0 +1,31 @@
+### GPAW installation script for Sisu (/Taito)
+###   uses --prefix to set a custom installation directory
+
+# version numbers (modify if needed)
+gpaw_version=1.4.0
+
+# installation directory (modify!)
+tgt=/appl/nano/gpaw/$gpaw_version
+
+# setup build environment
+module load scalable-python/1.2
+module load cray-hdf5-parallel
+module load libxc/3.0.0
+module load git
+export CRAYPE_LINK_TYPE=dynamic
+export CRAY_ADD_RPATH=yes
+
+# gpaw
+git clone https://gitlab.com/gpaw/gpaw.git gpaw-$gpaw_version
+cd gpaw-$gpaw_version
+git checkout $gpaw_version
+patch gpaw/test/test.py ../setup/patch-test.diff
+patch gpaw/test/xc/xc.py ../setup/patch-xc.diff
+ln -s ../setup/gcc.py
+python setup.py install --customize=../setup/customize-sisu.py --prefix=$tgt 2>&1 | tee loki-inst
+cd ..
+
+# fix permissions
+chmod -R g+rwX $tgt
+chmod -R o+rX $tgt
+
