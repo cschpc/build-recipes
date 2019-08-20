e9d0762a1b4a889d5b33f43eca5afe1b6a07da44
Martti Louhivuori <martti.louhivuori@csc.fi>
2019-08-20 10:46:07 +0300

    GPAW 1.4.0 with MPICH on Puhti

---
+++ gpaw/examples/puhti-1.4.0/setup/customize-puhti.py
@@ -0,0 +1,34 @@
+# Custom GPAW setup for Sisu (Cray XC40)
+import os
+
+# compiler
+compiler = './gcc.py'
+mpicompiler = './gcc.py'
+mpilinker = 'mpicc'
+extra_compile_args = ['-std=c99', '-O3', '-fopenmp-simd']
+#extra_link_args = ['-fno-lto']
+
+#library_dirs += ['/appl/soft/phys/gpaw/python-2.7.13/lib']
+
+# libz
+libraries = ['z']
+
+# libxc
+library_dirs += [os.environ['LIBXCDIR'] + '/lib']
+include_dirs += [os.environ['LIBXCDIR'] + '/include']
+libraries += ['xc']
+
+# MKL
+libraries += ['mkl_intel_lp64' ,'mkl_sequential' ,'mkl_core']
+mpi_libraries += ['mkl_scalapack_lp64', 'mkl_blacs_intelmpi_lp64']
+
+# use ScaLAPACK and HDF5
+scalapack = True
+hdf5 = True
+
+# GPAW defines
+define_macros += [('GPAW_NO_UNDERSCORE_CBLACS', '1')]
+define_macros += [('GPAW_NO_UNDERSCORE_CSCALAPACK', '1')]
+define_macros += [("GPAW_ASYNC",1)]
+define_macros += [("GPAW_MPI2",1)]
+
