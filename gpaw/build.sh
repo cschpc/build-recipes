1256a940a01c3c83f80c3f48596104843f26154a
Martti Louhivuori <martti.louhivuori@csc.fi>
2019-08-20 10:45:30 +0300

    Use MPICH for GPAW on Puhti

--- gpaw/build.sh
+++ gpaw/build.sh
@@ -10,7 +10,7 @@ tgt=/appl/soft/phys/gpaw/$gpaw_version
 
 # setup build environment
 module load gcc/9.1.0
-module load hpcx-mpi/2.4.0
+module load mpich/3.3.1
 module load hdf5/1.10.4
 module load intel-mkl/2019.0.4
 source /appl/soft/phys/gpaw/python/2.7.13/load.sh 2018-03
@@ -22,7 +22,7 @@ export LIBXCDIR=/appl/soft/phys/libxc/$libxc_version
 git clone https://gitlab.com/gpaw/gpaw.git gpaw-$gpaw_version
 cd gpaw-$gpaw_version
 git checkout $gpaw_version
-patch gpaw/test/test.py ../setup/patch-test.diff
+#patch gpaw/test/test.py ../setup/patch-test.diff
 patch gpaw/test/xc/xc.py ../setup/patch-xc.diff
 ln -s ../setup/gcc.py
 python setup.py install --customize=../setup/customize-puhti.py --prefix=$tgt 2>&1 | tee loki-inst
