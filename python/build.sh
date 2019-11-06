32d079cef3ed32e2f1ad817d931177e93569c1bd
Martti Louhivuori <martti.louhivuori@csc.fi>
2019-11-06 14:18:25 +0200

    Add explicit Tcl/Tk linking for GUI support

--- python/build.sh
+++ python/build.sh
@@ -11,22 +11,26 @@ git checkout v3.7.3
 # setup build environment
 module load gcc/9.1.0
 module load mpich/3.3.1
-module load hdf5/1.10.4
 module load intel-mkl/2019.0.4
 export CFLAGS='-march=cascadelake -O3'
 export CFLAGS="$CFLAGS -I/appl/spack/install-tree/gcc-9.1.0/libffi-3.2.1-5jujit/lib/libffi-3.2.1/include/"
 export FFLAGS=$CFLAGS
 export CPPFLAGS=$CFLAGS
 export LDFLAGS=-L/appl/spack/install-tree/gcc-9.1.0/libffi-3.2.1-5jujit/lib64
+export TCL_ROOT=/appl/spack/install-tree/gcc-9.1.0/tcl-8.6.8-ukn7zh
+export TK_ROOT=/appl/spack/install-tree/gcc-9.1.0/tk-8.6.8-qnfbzv
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$TCL_ROOT/lib:$TK_ROOT/lib"
+export TCL_LIBRARY=$TCL_ROOT/lib/tcl8.6
+export TK_LIBRARY=$TK_ROOT/lib/tk8.6
 
 # build and install
-./configure --prefix=$tgt --enable-shared --disable-ipv6 2>&1 | tee loki-conf
+./configure --prefix=$tgt --enable-shared --disable-ipv6 --with-tcltk-includes="-I$TCL_ROOT/include -I$TK_ROOT/include" --with-tcltk-libs="-L$TCL_ROOT/lib -L$TK_ROOT/lib -ltcl8.6 -ltk8.6" 2>&1 | tee loki-conf
 make 2>&1 | tee loki-make
 make install 2>&1 | tee loki-inst
 cd ..
 
 # setup load script
-sed -e "s|<BASE>|$tgt|g" load.sh > $tgt/load.sh
+sed -e "s|<BASE>|$tgt|g" -e "s|<TCLTK>|$TCL_ROOT/lib:$TK_ROOT/lib|g" -e "s|<TCLLIB>|$TCL_LIBRARY|g" -e "s|<TKLIB>|$TK_LIBRARY|g" load.sh > $tgt/load.sh
 
 # install pip
 source $tgt/load.sh
