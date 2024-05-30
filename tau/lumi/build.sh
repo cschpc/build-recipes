4e3e225c95437ba36f93700641beec6397789051
Juhana Lankinen <juhana.lankinen@csc.fi>
2024-05-30 11:16:05 +0300

    Add pdt download & install; Change compilers for mpi case

--- tau/lumi/build.sh
+++ tau/lumi/build.sh
@@ -18,6 +18,33 @@ fi
 base_dir=$(realpath $1)
 tau_dir=$base_dir/tau
 
+mkdir -p $tau_dir || \
+    { echo "Failed to create directory $tau_dir"; exit 1; }
+
+cd $tau_dir
+
+echo "Downloading PDT"
+pdt_tarball=pdt_lite.tgz
+pdt_url=https://cs.uoregon.edu/research/tau/$pdt_tarball
+pdt_dir=$tau_dir/pdtoolkit
+
+wget $pdt_url -N || \
+    { echo "Couldn't get files from $pdt_url"; exit 1; }
+
+mkdir -p $pdt_dir || \
+    { echo "Failed to create directory $pdt_dir"; exit 1; }
+
+tar -xf $pdt_tarball -C $pdt_dir --strip-components=2 --skip-old-files || \
+    { echo "Couldn't extract files from tarball $pdt_tarball"; exit 1; }
+
+cd $pdt_dir
+echo "Installing pdt"
+{ ./configure && make -j 8 && make install -j 8; } || \
+    { echo "Failed to install PDT"; exit 1; }
+
+cd $tau_dir
+
+echo "Downloading TAU"
 # Check if the second argument is a version number
 re='^[0-9]+([.][0-9]+)*$'
 if [ $# -eq 2 ] && [[ "$2" =~ $re ]]
@@ -26,14 +53,6 @@ then
 else
     version=latest
 fi
-
-install_dir=$tau_dir/$version
-
-mkdir -p $tau_dir || \
-    { echo "Failed to create directory $tau_dir"; exit 1; }
-
-cd $tau_dir
-echo "Downloading TAU"
 
 if [ "$version" = "latest" ]
 then
@@ -44,8 +63,10 @@ else
     tau_url=https://www.cs.uoregon.edu/research/tau/tau_releases/$tau_tarball
 fi
 
+install_dir=$tau_dir/$version
+
 wget $tau_url -N || \
-    { echo "Couldn't get tau from $tau_url"; exit 1; }
+    { echo "Couldn't get files from $tau_url"; exit 1; }
 
 mkdir -p $install_dir || \
     { echo "Failed to create directory $install_dir"; exit 1; }
@@ -54,6 +75,15 @@ tar -xf $tau_tarball -C $install_dir --strip-components=2 --skip-old-files || \
     { echo "Couldn't extract files from tarball $tau_tarball"; exit 1; }
 
 cd $install_dir
+
+echo "Downloading external dependencies"
+ext_deps_tarball=ext.tgz
+ext_deps_url=https://cs.uoregon.edu/research/tau/$ext_deps_tarball
+wget $ext_deps_url -N || \
+    { echo "Couldn't get files from $ext_deps_url"; exit 1; }
+
+tar -xf $ext_deps_tarball --skip-old-files || \
+    { echo "Couldn't extract files from tarball $ext_deps_tarball"; exit 1; }
 
 echo "Loading modules"
 # Change the versions as newer become available
@@ -72,33 +102,30 @@ ml rocm/$rocm_version
 
 echo "Configuring and installing"
 
-compiler_conf="-cc=cc -c++=CC -fortran=ftn"
+base_compiler_conf="-cc=cc -c++=CC -fortran=ftn"
+mpi_compiler_conf="-cc=mpicc -c++=mpicxx -fortran=mpif90"
 io_conf="-iowrapper"
-
-base_conf="-bfd=download -otf=download -unwind=download -dwarf=download $io_conf $compiler_conf"
-
+base_conf="-bfd=download -otf=download -unwind=download -dwarf=download"
 pthread_conf="-pthread"
 omp_conf="-openmp"
-
 python_conf="-python"
 mpi_conf="-mpi"
-
 papi_conf="-papi=/opt/cray/pe/papi/$papi_version"
 rocm_conf="-rocm=/opt/rocm-$rocm_version"
 rocprofiler_conf="-rocprofiler=/opt/rocm-$rocm_version/rocprofiler"
 roctracer_conf="-roctracer=/opt/rocm-$rocm_version/roctracer"
-pdt_conf="-pdt=download"
+pdt_conf="-pdt=$pdt_dir"
 external_packages_conf="$papi_conf $rocm_conf $rocprofiler_conf $roctracer_conf $pdt_conf"
 
 declare configs
 configs=(
-    "$base_conf" "$external_packages_conf"
-    "$base_conf" "$external_packages_conf" "$pthread_conf"
-    "$base_conf" "$external_packages_conf" "$pthread_conf" "$mpi_conf"
-    "$base_conf" "$external_packages_conf" "$pthread_conf" "$mpi_conf" "$python_conf"
-    "$base_conf" "$external_packages_conf" "$omp_conf"
-    "$base_conf" "$external_packages_conf" "$omp_conf" "$mpi_conf"
-    "$base_conf" "$external_packages_conf" "$omp_conf" "$mpi_conf" "$python_conf"
+    "$base_conf $io_conf $base_compiler_conf $external_packages_conf"
+    "$base_conf $io_conf $base_compiler_conf $external_packages_conf $pthread_conf"
+    "$base_conf $io_conf $mpi_compiler_conf  $external_packages_conf $pthread_conf $mpi_conf"
+    "$base_conf $io_conf $mpi_compiler_conf  $external_packages_conf $pthread_conf $mpi_conf $python_conf"
+    "$base_conf $io_conf $base_compiler_conf $external_packages_conf $omp_conf"
+    "$base_conf $io_conf $mpi_compiler_conf  $external_packages_conf $omp_conf $mpi_conf"
+    "$base_conf $io_conf $mpi_compiler_conf  $external_packages_conf $omp_conf $mpi_conf $python_conf"
 )
 
 for conf in "${configs[@]}"; do
