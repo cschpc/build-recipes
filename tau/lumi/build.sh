1535d1ab0f01d8c299b10b2fcf7232aabbb56a27
Juhana Lankinen <juhana.lankinen@csc.fi>
2024-06-11 15:20:49 +0300

    Update tau install script

--- tau/lumi/build.sh
+++ tau/lumi/build.sh
@@ -4,30 +4,50 @@
 # If you need to reinstall tau, it's probably best to remove the old directory altogether
 # and then rerun this script
 
-modification_date=2024-05-30
+echo_with_lines () {
+echo "--------------------------------------------------------------------------------"
+echo "$1"
+echo "--------------------------------------------------------------------------------"
+}
+
+
+modification_date=2024-06-11
 authors="Juhana Lankinen, "
 
-echo "This script downloads and installs TAU"
-echo "It's configured to work on LUMI"
-echo "Last update on $modification_date"
+echo_with_lines "This script downloads and installs TAU. It's configured to work on LUMI. Last update on $modification_date"
 
-if [ $# -eq 0 ] || [ ! -d "$1" ]
-then
-    echo "Usage: $0 /path/to/install/dir [tau version]"
-    echo "E.g. \"$0 /projappl/project_462000007/$USER/apps 2.33\""
-    echo "Or \"$0 /projappl/project_462000007/$USER/apps\" to install the latest version"
-    exit 1
-fi
+# Check arguments
+version_number_regexp='^[0-9]+([.][0-9]+)*$'
+([ $# -eq 2 ] && [ -d "$1" ] && [[ "$2" =~ $version_number_regexp ]]) || \
+    { echo "Usage: $0 /path/to/install/dir tau version"; \
+        echo "E.g. \"$0 /projappl/project_462000007/$USER/apps 2.33\"";
+        exit 1; }
 
 base_dir=$(realpath $1)
 tau_dir=$base_dir/tau
+version=$2
 
 mkdir -p $tau_dir || \
     { echo "Failed to create directory $tau_dir"; exit 1; }
 
 cd $tau_dir
 
-echo "Downloading PDT"
+echo_with_lines "Loading modules"
+# Change the versions as newer become available
+lumi_version=23.09
+python_version=3.10.10
+rocm_version=5.4.6
+papi_version=7.0.1.1
+
+ml LUMI/$lumi_version
+ml partition/G
+ml cray-python/$python_version
+ml rocm/$rocm_version
+
+rocm_path=/appl/lumi/SW/LUMI-$lumi_version/G/EB/rocm/$rocm_version
+[ -d $rocm_path ] || { echo "$rocm_path is not a directory"; exit 1; }
+
+echo_with_lines "Downloading PDT"
 pdt_tarball=pdt_lite.tgz
 pdt_url=http://tau.uoregon.edu/$pdt_tarball
 pdt_dir=$tau_dir/pdtoolkit
@@ -42,31 +62,16 @@ tar -xzf $pdt_tarball -C $pdt_dir --strip-components=2 --skip-old-files || \
     { echo "Couldn't extract files from tarball $pdt_tarball"; exit 1; }
 
 cd $pdt_dir
-echo "Installing pdt"
+
+echo_with_lines "Installing pdt"
 { ./configure && make -j 8 && make install -j 8; } || \
     { echo "Failed to install PDT"; exit 1; }
 
 cd $tau_dir
 
-echo "Downloading TAU"
-# Check if the second argument is a version number
-re='^[0-9]+([.][0-9]+)*$'
-if [ $# -eq 2 ] && [[ "$2" =~ $re ]]
-then
-    version=$2
-else
-    version=latest
-fi
-
-if [ "$version" = "latest" ]
-then
-    tau_tarball=tau.tgz
-    tau_url=https://www.cs.uoregon.edu/research/tau/$tau_tarball
-else
-    tau_tarball=tau-$version.tar.gz
-    tau_url=https://www.cs.uoregon.edu/research/tau/tau_releases/$tau_tarball
-fi
-
+echo_with_lines "Downloading TAU"
+tau_tarball=tau-$version.tar.gz
+tau_url=https://www.cs.uoregon.edu/research/tau/tau_releases/$tau_tarball
 install_dir=$tau_dir/$version
 
 wget $tau_url -N || \
@@ -80,7 +85,7 @@ tar -xzf $tau_tarball -C $install_dir --strip-components=2 --skip-old-files || \
 
 cd $install_dir
 
-echo "Downloading external dependencies"
+echo_with_lines "Downloading external dependencies"
 ext_deps_tarball=ext.tgz
 ext_deps_url=http://tau.uoregon.edu/$ext_deps_tarball
 wget $ext_deps_url -N || \
@@ -89,21 +94,7 @@ wget $ext_deps_url -N || \
 tar -xzf $ext_deps_tarball --skip-old-files || \
     { echo "Couldn't extract files from tarball $ext_deps_tarball"; exit 1; }
 
-echo "Loading modules"
-# Change the versions as newer become available
-lumi_version=23.09
-python_version=3.10.10
-rocm_version=5.4.6
-papi_version=7.0.1.1
-
-ml LUMI/$lumi_version
-ml partition/G
-ml cray-python/$python_version
-ml rocm/$rocm_version
-
-
-echo "Configuring and installing"
-
+echo_with_lines "Configuring and installing"
 base_compiler_conf="-cc=cc -c++=CC -fortran=ftn"
 io_conf="-iowrapper"
 base_conf="-bfd=download -otf=download -unwind=download -dwarf=download"
@@ -112,9 +103,9 @@ omp_conf="-openmp"
 python_conf="-python"
 mpi_conf="-mpi"
 papi_conf="-papi=/opt/cray/pe/papi/$papi_version"
-rocm_conf="-rocm=/opt/rocm-$rocm_version"
-rocprofiler_conf="-rocprofiler=$(which rocprof)"
-roctracer_conf="-roctracer=$(which roctrace)"
+rocm_conf="-rocm=$rocm_path"
+rocprofiler_conf="-rocprofiler=$rocm_path/rocprofiler"
+roctracer_conf="-roctracer=$rocm_path/roctracer"
 pdt_conf="-pdt=$pdt_dir"
 external_packages_conf="$papi_conf $rocm_conf $rocprofiler_conf $roctracer_conf $pdt_conf"
 
@@ -130,9 +121,9 @@ configs=(
 )
 
 for conf in "${configs[@]}"; do
-    echo "Configuring and installing with $conf"
+    echo_with_lines "Configuring and installing with $conf"
     (./configure $conf && make install -j 8) || \
         { echo "Failed to configure and install TAU with $conf"; exit 1; }
 done
 
-echo "TAU installation successful!"
+echo_with_lines "TAU installation successful!"
