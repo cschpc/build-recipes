79dbd9cbdfc782da07fb8ce5d2872e948338492e
Lauri Niemi <lauri.niemi@csc.fi>
2025-06-02 11:21:13 +0300

    Updated gpaw mahti build script for 25.1.0 and to include libvdwxc

--- gpaw/examples/mahti/setup/siteconfig-mahti-omp.py
+++ gpaw/examples/mahti/setup/siteconfig-mahti-omp.py
@@ -20,7 +20,6 @@ libraries += ['openblas']
 # scalapack
 scalapack = True
 libraries += ['scalapack']
-
 # fftw
 fftw = True
 libraries += ['fftw3']
@@ -28,6 +27,15 @@ libraries += ['fftw3']
 # libxc
 libraries += ['xc']
 dpath = '/appl/spack/v017/install-tree/gcc-11.2.0/libxc-5.1.5-oa6ihp'
+include_dirs += [f'{dpath}/include']
+library_dirs += [f'{dpath}/lib']
+extra_link_args += [f'-Wl,-rpath,{dpath}/lib']
+
+# libvdwxc
+libvdwxc = True
+libraries += ['vdwxc']
+# Not available in gcc/11.2.0 tree! Must take from gcc/13.1.0. Not ideal, but works
+dpath = '/appl/spack/v020/install-tree/gcc-13.1.0/libvdwxc-0.4.0-5vlzlb/'
 include_dirs += [f'{dpath}/include']
 library_dirs += [f'{dpath}/lib']
 extra_link_args += [f'-Wl,-rpath,{dpath}/lib']
