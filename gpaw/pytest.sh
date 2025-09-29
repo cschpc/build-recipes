c38a216f5d8f5b9a782637d121c49f6b7612d307
Tuomas Rossi <tuomas.rossi@csc.fi>
2025-09-29 08:29:13 +0300

    Do not run serial tests with srun

--- gpaw/pytest.sh
+++ gpaw/pytest.sh
@@ -80,8 +80,10 @@ function submit_job {
     tmp_dir=$run_dir/pytest_tmp
 
     sbatch_args="--mem-per-cpu=4G"
+    srun="srun"
     if [[ $n -eq 1 ]]; then
         sbatch_args="--mem-per-cpu=8G"
+        srun=""
     fi
 
     rm -rf $run_dir
@@ -91,7 +93,7 @@ function submit_job {
     ln -s $(readlink -f $test_dir) ./
     mkdir -p $cache_dir/d
     ln -s $(readlink -f $gpw_files) $cache_dir/d/
-    sbatch -J $name -o slurm.out -t 04:00:00 -N 1 -n $n --cpus-per-task=1 -p small $sbatch_args --wrap="gpaw info; srun $cmd --disable-pytest-warnings -o cache_dir=$cache_dir --basetemp=$tmp_dir $tests; rm -rf $tmp_dir"
+    sbatch -J $name -o slurm.out -t 04:00:00 -N 1 -n $n --cpus-per-task=1 -p small $sbatch_args --wrap="gpaw info; $srun $cmd --disable-pytest-warnings -o cache_dir=$cache_dir --basetemp=$tmp_dir $tests; rm -rf $tmp_dir"
     popd
 }
 
